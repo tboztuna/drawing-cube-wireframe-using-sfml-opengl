4149da251d6a51998c2c8a9f17546297d4c547e4
Tolga Boztuna <tboztuna@yahoo.com>
2018-04-15 22:21:15 +0300

    files

--- sfml_wireframe/main.cpp
+++ sfml_wireframe/main.cpp
@@ -1,91 +1,161 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
-#include <SFML/Audio.hpp>
+#include <cmath>
+#include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
+// Replacement for gluPerspective function.
+void perspectiveGL(GLdouble, GLdouble, GLdouble, GLdouble);
+void drawCube(void);
 
-int main(int, char const**)
+int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
+    // create the window
+    sf::Window window(sf::VideoMode(1024, 768), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    window.setVerticalSyncEnabled(true);
+    
+    glEnable(GL_TEXTURE_3D);
+    
+    // load resources, initialize the OpenGL states, ...
+    glMatrixMode(GL_PROJECTION);
+    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+    glLoadIdentity();
+    
+    sf::Vector2u wsize = window.getSize();
+    glViewport(0, 0, wsize.x, wsize.y);
+    perspectiveGL(60, (float)wsize.x/(float)wsize.y, 0.1f, 512.f);
+    
+    
+    
+    // run the main loop
+    bool running = true;
+    while (running)
     {
-        // Process events
+        // handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
+            if (event.type == sf::Event::Closed)
+            {
+                // end the program
+                running = false;
             }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
             }
         }
+        
+        
+        // Activate the window before using OpenGL commands.
+        // This is useless here because we have only one window which is
+        // always the active one, but don't forget it if you use multiple windows
+        window.setActive();
+        
+        // clear the buffers
+        glClearColor(0.3f, 0.3f, 0.3f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // draw...
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glPushMatrix();
+        
 
-        // Clear screen
-        window.clear();
+        // draw...
+        glMatrixMode(GL_TEXTURE_3D);
+        glLoadIdentity();
+        glTranslatef(0.f, 0.f, -5.f);
 
-        // Draw the sprite
-        window.draw(sprite);
+        drawCube();
 
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        glPopMatrix();
+        
+        GLenum err = glGetError();
+        
+        // end the current frame (internally swaps the front and back buffers)
         window.display();
     }
-
+    
+    // release resources...
+    
     return EXIT_SUCCESS;
+}
+
+void perspectiveGL( GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar )
+{
+    const GLdouble pi = 3.1415926535897932384626433832795;
+    GLdouble fW, fH;
+    
+    //fH = tan( (fovY / 2) / 180 * pi ) * zNear;
+    fH = tan( fovY / 360 * pi ) * zNear;
+    fW = fH * aspect;
+    
+    glFrustum( -fW, fW, -fH, fH, zNear, zFar );
+}
+
+void drawCube()
+{
+    glBegin(GL_QUADS);      // Draw The Cube Using quads
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);  // Top Right Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);  // Top Left Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);  // Bottom Left Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);  // Bottom Right Of The Quad (Top)
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(0.f, -1.f, 0.f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);  // Top Right Of The Quad (Bottom)
+    glNormal3f(0.f, -1.f, 0.f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);  // Top Left Of The Quad (Bottom)
+    glNormal3f(0.f, -1.f, 0.f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);  // Bottom Left Of The Quad (Bottom)
+    glNormal3f(0.f, -1.f, 0.f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);  // Bottom Right Of The Quad (Bottom)
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(0.f, 0.f, 1.f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);  // Top Right Of The Quad (Front)
+    glNormal3f(0.f, 0.f, 1.f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);  // Top Left Of The Quad (Front)
+    glNormal3f(0.f, 0.f, 1.f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);  // Bottom Left Of The Quad (Front)
+    glNormal3f(0.f, 0.f, 1.f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);  // Bottom Right Of The Quad (Front)
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(0.f, 0.f, -1.f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);  // Top Right Of The Quad (Back)
+    glNormal3f(0.f, 0.f, -1.f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);  // Top Left Of The Quad (Back)
+    glNormal3f(0.f, 0.f, -1.f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);  // Bottom Left Of The Quad (Back)
+    glNormal3f(0.f, 0.f, -1.f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);  // Bottom Right Of The Quad (Back)
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(-1.f, 0.f, 0.f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);  // Top Right Of The Quad (Left)
+    glNormal3f(-1.f, 0.f, 0.f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);  // Top Left Of The Quad (Left)
+    glNormal3f(-1.f, 0.f, 0.f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);  // Bottom Left Of The Quad (Left)
+    glNormal3f(-1.f, 0.f, 0.f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);  // Bottom Right Of The Quad (Left)
+    
+    glColor3f(1.0f,1.0f,1.0f);  // Color White
+    glNormal3f(1.f, 0.f, 0.f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);  // Top Right Of The Quad (Right)
+    glNormal3f(1.f, 0.f, 0.f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);  // Top Left Of The Quad (Right)
+    glNormal3f(1.f, 0.f, 0.f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);  // Bottom Left Of The Quad (Right)
+    glNormal3f(1.f, 0.f, 0.f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);  // Bottom Right Of The Quad (Right)
+    
+    glEnd();          // End Drawing The Cube
 }
